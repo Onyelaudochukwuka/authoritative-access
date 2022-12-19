a34e8f79a8c491ec3aa0d4c8dbd7f59314b6d5ee
Udoka <udochukwukaonyela@gmail.com>
2022-12-19 22:27:23 +0100

    removed unneccessary files

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,6 +1,2 @@
-export { default as Loader } from './Loader';
 export { default as TableRow } from './TableRow';
-export { default as SelectViews } from './SelectViews';
 export { default as TableHeader } from './TableHeader';
-export { default as SelectInput } from './SelectInput';
-export { default as SelectDate } from './SelectDate';
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,4 @@ export { default as Input } from './molecules/Input';
 export { default as PassWordInput } from './molecules/PassWordInput';
 export { default as Layout } from './molecules/Layout';
 export { default as Navbar } from './molecules/Navbar';
-export { default as UsersStats } from './molecules/UsersStats';
 export { default as Table } from './molecules/Table';
